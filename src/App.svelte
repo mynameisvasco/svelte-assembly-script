1a885d40307d96ce510cd9542d9309065f905e0c
Vasco Sousa <myemailvasco@gmail.com>
2021-08-18 17:08:26 +0100

    :tada: Begin of svelte-assembly-script

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+
+  import assembly from "../build/optimized.wasm";
+
+  let add;
+  let n1 = 0;
+  let n2 = 0;
+  let res = 0;
+
+  onMount(async () => {
+    let imports;
+    const { instance } = await assembly({ ...imports });
+    add = instance.exports.add;
+  });
+</script>
+
+{res}
+<input bind:value={n1} />
+<input bind:value={n2} />
+<button on:click={() => (res = add(n1, n2))}> Add </button>
